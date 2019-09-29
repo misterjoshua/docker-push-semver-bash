df5416feb93b5da7701078238ee99bd6a29ec1bd
Josh Kellendonk <josh@colloco.com>
2019-09-29 21:05:24 +0000

    Remove unneeded here-string test

--- docker_push_semver_test.sh
+++ docker_push_semver_test.sh
@@ -11,9 +11,6 @@ function die() {
     exit 1
 }
 
-TEST="test"
-grep "t.*st" <<<$TEST || die "here-string doesn't work in this bash"
-
 dockerPushSemver "test-1" "1.2.3-test" | grep "push.*test-1:latest\$" >/dev/null || die "It didn't push test-1:latest"
 dockerPushSemver "test-1" "1.2.3-test" | grep "push.*test-1:1.2.3-test\$" >/dev/null || die "It didn't push test-1:1.2.3-test"
 dockerPushSemver "test-1" "1.2.3-test" | grep "push.*test-1:1.2.3\$" >/dev/null && die "It pushed test-1:1.2.3 with a SPECIAL"
