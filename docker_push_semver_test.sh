f0dda556ae9b085e559a6f7585910dc7af7ce801
Josh Kellendonk <josh@colloco.com>
2019-09-29 21:02:51 +0000

    Test with direct pipe

--- docker_push_semver_test.sh
+++ docker_push_semver_test.sh
@@ -14,17 +14,13 @@ function die() {
 TEST="test"
 grep "t.*st" <<<$TEST || die "here-string doesn't work in this bash"
 
-OUTPUT=$(dockerPushSemver "test-1" "1.2.3-test")
-cat <<<$OUTPUT
-grep "push.*test-1:latest" <<<$OUTPUT >/dev/null || die "It didn't push test-1:latest"
-grep "push.*test-1:1.2.3-test\$" <<<$OUTPUT >/dev/null || die "It didn't push test-1:1.2.3-test"
-grep "push.*test-1:1.2.3\$" <<<$OUTPUT >/dev/null && die "It pushed test-1:1.2.3 with a SPECIAL"
-grep "push.*test-1:1.2\$" <<<$OUTPUT >/dev/null && die "It pushed test-1:1.2 with a SPECIAL"
-grep "push.*test-1:1\$" <<<$OUTPUT >/dev/null && die "It pushed test-1:1 with a SPECIAL"
+dockerPushSemver "test-1" "1.2.3-test" | grep "push.*test-1:latest\$" >/dev/null || die "It didn't push test-1:latest"
+dockerPushSemver "test-1" "1.2.3-test" | grep "push.*test-1:1.2.3-test\$" >/dev/null || die "It didn't push test-1:1.2.3-test"
+dockerPushSemver "test-1" "1.2.3-test" | grep "push.*test-1:1.2.3\$" >/dev/null && die "It pushed test-1:1.2.3 with a SPECIAL"
+dockerPushSemver "test-1" "1.2.3-test" | grep "push.*test-1:1.2\$" >/dev/null && die "It pushed test-1:1.2 with a SPECIAL"
+dockerPushSemver "test-1" "1.2.3-test" | grep "push.*test-1:1\$" >/dev/null && die "It pushed test-1:1 with a SPECIAL"
 
-OUTPUT=$(dockerPushSemver "test-2" "1.2.3")
-cat <<<$OUTPUT
-grep "push.*test-2:latest\$" <<<$OUTPUT >/dev/null || die "It didn't push test-2:latest"
-grep "push.*test-2:1.2.3\$" <<<$OUTPUT >/dev/null || die "It didn't push test-2:1.2.3"
-grep "push.*test-2:1.2\$" <<<$OUTPUT >/dev/null || die "It didn't push test-2:1.2"
-grep "push.*test-2:1\$" <<<$OUTPUT >/dev/null || die "It didn't push test-2:1"
+dockerPushSemver "test-2" "1.2.3" | grep "push.*test-2:latest\$" >/dev/null || die "It didn't push test-2:latest"
+dockerPushSemver "test-2" "1.2.3" | grep "push.*test-2:1.2.3\$" >/dev/null || die "It didn't push test-2:1.2.3"
+dockerPushSemver "test-2" "1.2.3" | grep "push.*test-2:1.2\$" >/dev/null || die "It didn't push test-2:1.2"
+dockerPushSemver "test-2" "1.2.3" | grep "push.*test-2:1\$" >/dev/null || die "It didn't push test-2:1"
